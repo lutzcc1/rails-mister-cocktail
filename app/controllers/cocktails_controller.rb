b27aa9d7a154c6e619b912ca96536b8b93b8480e
Luis Cesar <lutzcc1@gmail.com>
2020-04-23 09:24:20 -0500

    add edit/update actions - refractor controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,12 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :set_task, only: %i[show edit update]
 
   def index
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
+  def show; end
 
   def new
     @cocktail = Cocktail.new
@@ -14,8 +13,6 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    # why I'm i getting empty elements in the :ingredient_ids string?
-    params[:cocktail][:ingredient_ids].reject!(&:empty?)
     params[:cocktail][:name] = params[:cocktail][:name].downcase.titleize
 
     @cocktail = Cocktail.new(cocktail_params)
@@ -24,13 +21,29 @@ class CocktailsController < ApplicationController
     redirect_to cocktails_path
   end
 
+  def edit; end
+
+  def update
+    @cocktail.update(cocktail_params)
+    @cocktail.save
+    add_doses
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
+
+  def set_task
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
 
   def add_doses
+     # why I'm i getting empty elements in the :ingredient_ids string?
+    params[:cocktail][:ingredient_ids].reject!(&:empty?)
+    @cocktail.doses.destroy_all
     ingredient_list = params[:cocktail][:ingredient_ids]
     ingredient_list.each_with_index do |ingredient, num_of_description|
       dose_description = params[:cocktail][:doses_attributes][num_of_description.to_s.to_sym][:description]
