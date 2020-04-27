0bbc8de139be6c15ac1bdfdbbae497193eab0a29
Luis Cesar <lutzcc1@gmail.com>
2020-04-27 16:22:57 -0500

    modified application.js to fix heroku JS 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ require("channels");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery3
